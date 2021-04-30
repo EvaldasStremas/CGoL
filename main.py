0146321e73c256e1b3a691ca4ae0c13650f5b59a
EvaldasStremas <evaldas.stremas123@gmail.com>
2021-04-30 13:29:44 +0300

    Writing logic

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-NUM_ROWS = 5
-NUM_COLS = 5
+NUM_ROWS = 6
+NUM_COLS = 6
 EMPTY_SYMBOL = "O"
 FILL_SYMBOL = "X"
 START_POINT_ROWS = 2
 START_POINT_COLS = 2
+
 
 def get_empty_grid(num_cols, num_rows, empty_symbol):
     grid = []
@@ -15,39 +16,59 @@ def get_empty_grid(num_cols, num_rows, empty_symbol):
         grid.append(row)
     return grid
 
+
 def print_grid(grid):
     for i in range(NUM_ROWS):
         print(grid[i])
 
-def replace_grid(grid, START_POINT_COLS, FISTART_POINT_ROWS):
-    grid[START_POINT_COLS][FISTART_POINT_ROWS] = FILL_SYMBOL
+
+def replace_grid(grid, START_POINT_COLS, START_POINT_ROWS):
+    grid[START_POINT_COLS][START_POINT_ROWS] = FILL_SYMBOL
     return grid
+
 
 # ------------------------------------MAIN-----------------------------------------
 def main():
-    # Plot
     grid = get_empty_grid(NUM_COLS, NUM_ROWS, EMPTY_SYMBOL)
+    calculater_grid = grid
+    grid = replace_grid(grid, START_POINT_COLS, START_POINT_ROWS)
 
-    replace_grid(grid, START_POINT_COLS, START_POINT_ROWS)
+    # grid[1][1] = 'X'
+    # grid[1][2] = 'X'
+    # grid[1][6] = 'X'
+    # grid[1][7] = 'X'
+    # # grid[2][7] = 'X'
+    # grid[1][8] = 'X'
+    grid[2][3] = 'X'
+    grid[2][4] = 'X'
 
-    # around_point = [
-    # grid[START_POINT_COLS-1][START_POINT_ROWS-1],
-    # grid[START_POINT_COLS-1][START_POINT_ROWS],
-    # grid[START_POINT_COLS-1][START_POINT_ROWS+1],
-    # grid[START_POINT_COLS][START_POINT_ROWS-1],
-    # grid[START_POINT_COLS][START_POINT_ROWS+1],
-    # grid[START_POINT_COLS+1][START_POINT_ROWS-1],
-    # grid[START_POINT_COLS-1][START_POINT_ROWS],
-    # grid[START_POINT_COLS+1][START_POINT_ROWS+1]
-    # ]
+    print_grid(grid)
 
-    for i in range(len(around_point)):
-        around_point[i] = FILL_SYMBOL
+    for x in range(NUM_ROWS):
+        for y in range(NUM_COLS):
 
-    print(around_point)
+            if grid[x][y] == FILL_SYMBOL:
+                around_point = [
+                    grid[x - 1][y - 1],
+                    grid[x - 1][y],
+                    grid[x - 1][y + 1],
+                    grid[x][y - 1],
+                    grid[x][y + 1],
+                    grid[x + 1][y - 1],
+                    grid[x + 1][y],
+                    grid[x + 1][y + 1]
+                ]
 
-    # print(grid)
-    # print_grid(grid)
+                x_counter = around_point.count(FILL_SYMBOL)
+
+                print(x_counter)
+
+                if x_counter == 2:
+                    calculater_grid[x][y] = FILL_SYMBOL
+
+    for i in range(NUM_ROWS):
+        print(calculater_grid[i])
+
 
 if __name__ == '__main__':
     main()
